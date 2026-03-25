2b7f9c775bf0bbd2885ffbde8136c3913044d11e
askaa <azkalahmasanew@gmail.com>
2026-03-25 20:35:43 +0800

    Update custom_courbet.mk

--- custom_courbet.mk
+++ custom_courbet.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_courbet
 PRODUCT_DEVICE := courbet
